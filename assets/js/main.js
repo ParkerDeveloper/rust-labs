ab0365bc95cde3a848263be11bed70104b607411
Артём Ермаков <79099792+Alfedov@users.noreply.github.com>
2023-03-21 13:49:20 +0300

    New function

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,33 @@
 //   .catch((error) => {
 //     console.error("Error:", error);
 //   });
+
+//document.querySelector("div[ui-view]").style.display="none"
+//var newDiv = document.createElement("div");
+//
+//var target_div = document.querySelector("div[ui-view]");
+//target_div.parentNode.insertBefore(newDiv, target_div.nextSibling);
+//newDiv.setAttribute("id", "root");
+
+const querySelectors = {
+    product: document.querySelector('.products'),
+    category: document.querySelector('.categories'),
+}
+
+const apiRequestShop = async () => {
+    return await new Promise((res) => {
+        const request = new XMLHttpRequest();
+
+        request.open('POST', `${window.location.origin}/files/stores/backend/store.shop.php`);
+        request.setRequestHeader('Accept',"application/json");
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        request.onload = () => {
+            res(JSON.parse(request.response));
+            console.log(request.response)
+        }
+
+        request.send(JSON.stringify({
+            action: "info"}));
+    });
+}
